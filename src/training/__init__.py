aa8d2627617d9197ec515e38e142e590aea19450
khoacaoFizo <khoa.cao@fizo.vn>
2026-02-04 14:22:24 +0700

    Remove src from tracking and ignore it

--- src/training/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Training module containing the Trainer class."""
-from src.training.trainer import Trainer
-
-__all__ = ["Trainer"]
--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utility functions."""
-from src.utils.common import seed_everything
-from src.utils.postprocess import decode_with_confidence
-
-__all__ = ["seed_everything", "decode_with_confidence"]
